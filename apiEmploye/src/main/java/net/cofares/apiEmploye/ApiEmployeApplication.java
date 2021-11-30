253fb3772b7d5e360f92d2980ba06f3d5678d4c3
Pascal Fares <pascal.fares@cofares.net>
2021-11-30 15:45:59 +0100

    Les starter , initialisation

---
+++ apiEmploye/src/main/java/net/cofares/apiEmploye/ApiEmployeApplication.java
@@ -0,0 +1,13 @@
+package net.cofares.apiEmploye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmployeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEmployeApplication.class, args);
+	}
+
+}
---
+++ apiEmploye/src/test/java/net/cofares/apiEmploye/ApiEmployeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cofares.apiEmploye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmployeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
